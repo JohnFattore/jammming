30b00c5cb227b5a2bec9024b66f2555b3b99a1bb
John Fattore <105743305+JohnFattore@users.noreply.github.com>
2024-03-10 15:49:58 -0500

    Update Playlist.js

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -95,12 +95,6 @@ function PlayList(props) {
         }
     }
     //
-    function sleep(miliseconds) {
-        var currentTime = new Date().getTime();
-     
-        while (currentTime + miliseconds >= new Date().getTime()) {
-        }
-     }
 
     async function fattorePlaylist() {
         try {
